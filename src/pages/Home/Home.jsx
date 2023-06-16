a4aebbcf08a4406ef48903e0553ac459a40e032a
Coral Jimenez <info.coraljimenez@gmail.com>
2023-06-16 20:23:11 +0200

    Feat: Folders added with her basi route. Style: some basic colors nd typografy added

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+import React, {useState, useEffect} from 'react';
+import "./Home.css";
+// import { bringProducts } from '../../services/apiCalls';
+// import { ProductCard } from '../../common/ProductCard/ProductCard';
+ 
+export const Home = () => {
+    return (
+        <div className='homeDesign'></div>
+    )
+}
