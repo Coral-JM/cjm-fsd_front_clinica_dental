eb6d7b1ed39dedd6b2a4403f8d5eceb0452a7dce
Coral Jimenez <info.coraljimenez@gmail.com>
2023-06-21 19:41:40 +0200

    Feat: Treatments views uploaded

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,21 +2,23 @@
 import React, {useState, useEffect} from 'react';
 import "./Home.css";
 import imgHome from '../../img/modelImg.jpg';
-// import { ProductCard } from '../../common/ProductCard/ProductCard';
  
 export const Home = () => {
     return (
-        <div className='homeDesign'>
-            <div className="tittle">( m i n t )</div>
-            <div className="content">
+        <div className='container'>
+            <div className="homeDesign row">
+                <div className="tittle">( m i n t )</div>
+                <div className="textHome col-6">
                 <div className="tittleTwo">Reinventando</div>
                 <div className="tittleTwo">la cultura de</div>
                 <div className="tittleTwo">la salud bucal</div>
-
                 <div className="subtittleHome">( m i n t ) es bienestar, belleza y cuidado.</div>
                 <div className="subtittleHome">Un espacio donde cuidaremos de ti y de tu sonrisa.</div>  
-                <img src={ imgHome } className='imgHome'/>
-                <div className="pideCitaHome" onClick={()=>navigate("/pidecita")}>Reserva tu cita</div>
+                <div className="pideCitaHome col-3 " onClick={()=>navigate("/pidecita")}>Reserva tu cita</div>
+                </div>
+                <div className="col-6 imgHome">
+                <img src={ imgHome } className="imgHome"/>
+                </div>
             </div>
         </div>
     )
