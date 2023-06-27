62395f958701852b0cf930d271c1e3772929b225
Coral Jimenez <info.coraljimenez@gmail.com>
2023-06-27 17:49:13 +0200

    Fix: logout working in profile view

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import {Container, Row, Col} from "react-bootstrap"
 import "./Profile.css";
 import { logout } from "../../pages/userSlice";
+import { useDispatch } from "react-redux";
 
 
 
 export const Profile = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
     
     return (
-        <div className="container">
-            <div className="row profileBody">
-                <div className="col profileTittle">( perfil de usuario )</div>
-
+        <Container>
+            <Row className="profileBody">
+                <Col>
+                <div className="profileTittle">( perfil de usuario )</div>
                 <div onClick={()=>navigate("/UpdateUser")} style= {{width:"24em"}}className="profileLines">Información del usuario</div>
                 <div onClick={()=>navigate("/appointmentsasuser")} style= {{width:"24em"}}className="profileLines">Mis citas</div> 
-                <div onClick={()=>{dispatch(logout()); navigate("/home")}} style= {{width:"24em"}}className="profileLinesEnd">Log out</div>
-
-            </div>
-        </div>
+                <div onClick={()=>{
+                  dispatch(logout()); 
+                  navigate("/home")
+                  }} style= {{width:"24em"}}className="profileLines">Log out</div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
--- src/pages/UpdateUser/UpdateUser.jsx
+++ src/pages/UpdateUser/UpdateUser.jsx
@@ -5,20 +5,17 @@ export const UpdateUser = () => {
     return (
         <div className="container">
             <div className="row userBody">
-                <div className="col userTittle"> nombre del usuario</div>
+                <div className="col userTittle">nombre del usuario</div>
 
                 <div style= {{width:"24em"}}className="userLines">Nombre y apellido</div>
                 <div style= {{width:"24em"}}className="userApi">Adriana Bausach</div>
-                {/* Este último div deberá ser un input text con los datos del usuario que aparezcan por defecto según el usuario y tendrá que tener habilitada la opción de modificación */}
 
                 <div style= {{width:"24em"}}className="userLines">Email</div> 
                 <div style= {{width:"24em"}}className="userApi">adribau@gmail.com</div> 
-                {/* Este último div deberá ser un input text con los datos del usuario que aparezcan por defecto según el usuario y tendrá que tener habilitada la opción de modificación */}
 
 
                 <div style= {{width:"24em"}}className="userLines">Password</div>
                 <div style= {{width:"24em"}}className="userApi">***********</div>
-                {/* Este último div deberá ser un input text con los datos del usuario que aparezcan por defecto según el usuario y tendrá que tener habilitada la opción de modificación */}
 
                 <div style= {{width:"15em"}}className="modificarDatos">Modificar datos</div>
                 
