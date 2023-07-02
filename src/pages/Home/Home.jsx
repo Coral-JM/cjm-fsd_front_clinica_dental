46184adfea5613b1cba7cf71b26f0a2479c84e6f
Coral Jimenez <info.coraljimenez@gmail.com>
2023-07-02 15:57:51 +0200

    Feat: validation in create appointment from home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,25 @@
 
 import React, {useState, useEffect} from 'react';
+import { useSelector } from "react-redux";
 import "./Home.css";
 import { Container, Row, Col  } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import imgHome from '../../img/modelImg.jpg';
 import { useNavigate } from 'react-router-dom';
+import { userData } from "../../pages/userSlice";
  
 export const Home = () => {
-
+    const datos = useSelector(userData);
+    const isLogged = datos?.credentials?.token
     const navigate = useNavigate();
+  
+    const handleReservationClick = () => {
+      if (isLogged) {
+        navigate('/createappointment');
+      } else {
+        navigate('/login');
+      }
+    };
     
     return (
         <Container>
@@ -21,8 +32,14 @@ export const Home = () => {
                     <Card.Text style={{ fontSize:"1em"}} className='subtittleHome'>
                     ( m i n t ) es bienestar, belleza y cuidado. Un espacio donde cuidaremos de ti y de tu sonrisa.
                     </Card.Text>
+
+
                     <div style={{ fontSize:"1em", background: "#15aabf", border: "transparent", fontFamily: "monospace", margin: "1em"}} className='citaButton'
-                    onClick={()=>navigate("/createappointment")}>Reserva tu cita</div>
+                    onClick={handleReservationClick}>Reserva tu cita</div>
+
+
+
+
                     </Card.Body>
                 </Card>
                 </Col>
